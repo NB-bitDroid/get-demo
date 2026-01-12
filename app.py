39a2833e50c5779748bb3a453ebb518a4c2fb029
NB-bitDroid <bennettnicolasm@gmain.com>
2026-01-12 08:41:44 -0500

    added more stuff to app

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("Hello I'm here for git practice - Local")
+print("Sometiomes I think about the frea, of being a walmart greeter")
 print("Hello I'm here for git practice - Remote")
+print("Sometimes I relize I want to just play with electronics all day.")
+print("Added during merge")
+print("Local end")
 
